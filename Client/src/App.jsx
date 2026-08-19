3fae5e6bdc85eb7199e4efa00d6cec3decbd678c
Amrita7974 <amritakumari9343@gmail.com>
2026-08-19 21:08:53 +0530

    feat: implement authentication context and registration page, update app routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import Home from './pages/Home'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+import ContactUs from "./pages/Contactus";
+import UserDashboard from "./pages/UserDashboard";
 
 const App = () => {
+  const path = useLocation().pathname;
+  console.log(path);
+
   return (
-   <>
-     <Header/>
-     <Home/>
-   </>
-  )
-}
+    <>
+      <Toaster />
+      {/* {path !== "/chat" && <SiteHeader />} */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
